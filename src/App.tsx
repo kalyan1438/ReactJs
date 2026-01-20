04b11b903a65fd624c9264905ce10c73eb6cbf1e
Kalyan Rao <kalyanrao135@gmail.com>
2026-01-20 11:19:29 +0530

    icon added

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,8 @@
-
+import Navbar from "./components/Navbar";
+import Home from "./components/Home";
 export default function App(){
   return <>
-    <h1 className="bg-blue-400 text-5xl ">Hello World</h1>
+        <Navbar/>
+        <Home/>
     </>
 }
