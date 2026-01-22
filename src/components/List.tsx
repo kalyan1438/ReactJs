e7cff92afa8417dd1c456533dea48edb53bbc221
Kalyan Rao <kalyanrao135@gmail.com>
2026-01-22 15:00:51 +0530

    Remove added

--- src/components/List.tsx
+++ src/components/List.tsx
@@ -4,14 +4,22 @@ import { Button } from "./ui/button";
 export default function List(){
     const [list,setList] = useState<string[]>([]);
     const [item,setItem] = useState("");
+    const valid = ()=>{
+        if(item ==="") return;
+        setList([...list,item]);
+    }
+    const rem = (idx:number)=>{
+        setList(list.filter((_,i)=>i!==idx))
+    }
     return <>
     <Input placeholder="Enter Element" value={item} onChange={(e)=>setItem(e.target.value)}/>
-        <Button onClick={()=>setList([...list,item])}>Add</Button>
-        <ul>
+        <Button onClick={valid}>Add</Button>
+        {list.length>0 &&
+        <ul className="list-decimal list-inside">
         {list.map((i, idx) => (
-        <li key={idx}>{i}</li>
+         <li key={idx}>{i} <Button onClick={()=>rem(idx)} variant={"destructive"}>Remove</Button></li>
             ))}
         </ul>
-
-        </>
+        }
+         </>
 }
