977ce6930824c5a55c633819f1184eb25a0dabf8
Kalyan Rao <kalyanrao135@gmail.com>
2026-01-27 15:41:23 +0530

    ..

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { Route,Router,Routes } from "react-router-dom"
+import { Route,Routes } from "react-router-dom"
 import Navbar from "./components/Navbar"
 import Footer from "./components/Footer"
 import Home from "./pages/Home"
