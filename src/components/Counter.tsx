039187f581547d9cfbc19a07244c30e47db4b5fb
Kalyan Rao <kalyanrao135@gmail.com>
2026-01-29 15:57:27 +0530

    Redux counter

---
+++ src/components/Counter.tsx
@@ -0,0 +1,28 @@
+import { useSelector, useDispatch } from "react-redux";
+import {
+  increment,
+  decrement,
+  incrementByAmount,
+} from "../redux/slices/postSlice";
+import { useEffect } from "react";
+
+const Counter = () => {
+  const count = useSelector((state:any) => state.counter.value);
+  const dispatch = useDispatch();
+    useEffect(()=>{
+        console.log(count)
+    })
+  return (
+    <div>
+      <h2>Count: {count}</h2>
+
+      <button onClick={() => dispatch(increment())}>+</button>
+      <button onClick={() => dispatch(decrement())}>-</button>
+      <button onClick={() => dispatch(incrementByAmount(5))}>
+        +5
+      </button>
+    </div>
+  );
+};
+
+export default Counter;
